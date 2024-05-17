11fa652737682345541136b3817026a7f130cd4d
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-05-17 13:30:38 +0330

    creating promote and articles

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Navbar from "@/components/modules/navbar/Navbar";
+import Articles from "@/components/templates/index/articles/Articles";
 import Baner from "@/components/templates/index/baner/Baner";
 import Latest from "@/components/templates/index/latest/Latest";
+import Promote from "@/components/templates/index/promote/Promote";
 
 
 export default function Home() {
@@ -9,6 +11,8 @@ export default function Home() {
       <Navbar />
       <Baner/>
       <Latest/>
+      <Promote/>
+      <Articles/>
     </>
   )
 }
