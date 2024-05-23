e589232a6cb9ae36097548cfaf881917d188ffea
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-05-23 11:52:41 +0330

    validations email , phone , password

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -37,10 +37,29 @@ const generateRefreshToken =( data)=>{
     return token
 }
 
+const validateEmail = (email) =>{
+    
+    const pattern =/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/g
+    return pattern.test(email)
+}
+const validatePhone = (phone) =>{
+    
+    const pattern =/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/g
+    return pattern.test(phone)
+}
+const validatePassword = (password) =>{
+    
+    const pattern =/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/g
+    return pattern.test(password)
+}
+
 export {
     hashPassword,
     verifyPassword,
     generateAccessToken,
     verifyAccessToken,
-    generateRefreshToken
+    generateRefreshToken,
+    validateEmail,
+    validatePhone,
+    validatePassword
 }
