1d1dc64e6d6cd66bfda30ae01d44612a419fe503
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-07-07 09:46:20 +0330

    create user panel layout

---
+++ src/app/p-user/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserPanelLayout from "@/components/layouts/UserPanelLayout";
+
+function Index() {
+  return (
+    <UserPanelLayout>
+      <h1>Home Page</h1>
+    </UserPanelLayout>
+  );
+}
+
+export default Index;
---
+++ src/components/layouts/UserPanelLayout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./userPanelLayout.module.css";
+import Sidebar from "@/components/modules/p-user/Sidebar";
+import Topbar from "@/components/modules/p-user/Topbar";
+
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.layout}>
+      <section className={styles.section}>
+        <Sidebar />
+        <div className={styles.contents}>
+          <Topbar />
+          {children}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Layout;
