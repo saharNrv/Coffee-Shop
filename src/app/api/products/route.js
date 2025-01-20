b3c90e165759d2245b658422e855960859f6639c
saharNrv <122102473+saharNrv@users.noreply.github.com>
2025-01-20 13:49:46 +0330

    uploader image

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,5 +1,7 @@
 import connectToDB from "@/configs/db";
 import productModel from "@/models/Product";
+import { writeFile } from "fs/promises";
+import path from "path";
 
 export async function POST(req) {
     
@@ -43,9 +45,45 @@ export async function POST(req) {
     }
 }
 
+// Image Uploader
+export async function PUT(req) {
+    const formData = await req.formData();
+    const img = formData.get("img");
+  
+    // Validation
+    if (!img) {
+      return Response.json(
+        { message: "Product has not image !!" },
+        { status: 400 }
+      );
+    }
+  
+    try {
+      const buffer = Buffer.from(await img.arrayBuffer());
+      const filename = Date.now() + img.name;
+  
+      await writeFile(
+        path.join(process.cwd(), "public/uploads/" + filename),
+        buffer
+      );
+  
+      // ✅
+      return Response.json(
+        { message: "File uploaded successfully :))" },
+        { status: 201 }
+      );
+    } catch (err) {
+      console.log(err);
+      return Response.json({ message: err.message }, { status: 500 });
+    }
+  }
+  
+
 
 export async function GET (){
     const products = await productModel.find({},"-__v").populate("comments")
 
     return Response.json(products)
 }
+
+ 
